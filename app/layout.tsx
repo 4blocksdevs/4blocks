fa0385babebd9972b63dc6344c5350d2a3b4648f
4blocksdev <4blocksdevs@gmail.com>
2025-10-06 17:26:49 +0300

    fix: Replace dynamic Google Tag Manager ID with a hardcoded value in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,15 +68,15 @@ export default function RootLayout({
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5N39QMDC');`,
           }}
         />
 
         {/* Google Analytics 4 */}
-        <Script
+        {/* <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
         />
@@ -94,7 +94,7 @@ export default function RootLayout({
               });
             `,
           }}
-        />
+        /> */}
 
         {/* Meta Pixel */}
         <Script
@@ -155,7 +155,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-5N39QMDC`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
